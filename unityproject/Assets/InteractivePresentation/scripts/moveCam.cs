327cda1430308e6e37994c25d7a587f791ab8171
Mikipomaid <cakemas0227@icloud.com>
2019-11-18 01:11:31 +0900

    wip edit osc

--- unityproject/Assets/InteractivePresentation/scripts/moveCam.cs
+++ unityproject/Assets/InteractivePresentation/scripts/moveCam.cs
@@ -1,16 +1,22 @@
 ﻿using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
-using System.Linq;
 using UniRx;
 using UniRx.Triggers;
+using OscJack;
 
 public class moveCam : MonoBehaviour
 {
     [SerializeField]
     private Transform camRoot;
+    
+    private OscServer _server;
+    private Vector2 _touch;
+    
     private void Start()
     {
+        linkOsc();
+        
         this.UpdateAsObservable()
             .Where(_ => Input.GetKeyDown(KeyCode.A))
             .Subscribe(_ =>nextSlide());
@@ -28,5 +34,19 @@ public class moveCam : MonoBehaviour
     private void backSlide()
     {
         Debug.Log("back");
+    }
+
+    private void linkOsc()
+    {
+        _server = new OscServer(6000); // Port number
+        
+        _server.MessageDispatcher.AddCallback(
+            "/ZIGSIM/mikipomaid/touch",
+            (string address, OscDataHandle data) =>
+            {
+                _touch.x = data.GetElementAsFloat(0);
+                _touch.y = data.GetElementAsFloat(1);
+            }
+        );
     }
 }
