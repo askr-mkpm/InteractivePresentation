14589ab377caa425194aa308636f5be40d2e9d78
Mikipomaid <cakemas0227@icloud.com>
2019-11-19 04:31:52 +0900

    wip effect

--- unityproject/Assets/InteractivePresentation/scripts/moveCam.cs
+++ unityproject/Assets/InteractivePresentation/scripts/moveCam.cs
@@ -4,89 +4,81 @@ using UniRx;
 using UniRx.Triggers;
 using OscJack;
 
-public class moveCam : MonoBehaviour
+namespace InteractivePresentation.scripts
 {
-    [SerializeField]
-    private Transform camRoot;
-    
-    private GameObject _targetPos;
-
-    [SerializeField, Range(0, 100)] 
-    private float moveRange = 25;
-    
-    [SerializeField, Range(1, 10)] 
-    private float moveSpeed =1;
-    
-    private OscServer _server;
-    private Vector2 _touch;
-    private int _touchCount;
-
-    private void Awake()
+    public class MoveCam : MonoBehaviour
     {
-        _targetPos = new GameObject("targetPos");
-        _targetPos.transform.position = Vector3.zero;
-    }
+        [SerializeField] private Transform camRoot;
+        private GameObject _targetPos;
+        [SerializeField, Range(0, 100)] private float moveRange = 25;
+        [SerializeField, Range(1, 10)] private float moveSpeed = 1;
 
-    private void Start()
-    {
-        linkOsc();
-        
-        this.UpdateAsObservable()
-            .Where(_ => _touch.y > 0 && _touchCount == 1)
-            .ThrottleFirst(TimeSpan.FromSeconds(0.2))
-            .Subscribe(_ =>nextSlide(_targetPos.transform, moveRange));
-        
-        this.UpdateAsObservable()
-            .Where(_ => _touch.y < 0 && _touchCount == 1)
-            .ThrottleFirst(TimeSpan.FromSeconds(0.2))
-            .Subscribe(_ =>backSlide(_targetPos.transform, moveRange));
+        private OscServer _server;
+        private Vector2 _touch;
+        private int _touchCount;
 
-        this.UpdateAsObservable()
-            .Subscribe(_ => moveCamRoot(camRoot, _targetPos.transform));
-    }
+        private void Awake()
+        {
+            _targetPos = new GameObject("targetPos");
+            _targetPos.transform.position = Vector3.zero;
+        }
 
-    private void nextSlide(Transform target, float range)
-    {
-        Vector3 currentPos = target.position;
-        Vector3 nextPos = new Vector3(currentPos.x, currentPos.y, currentPos.z + range);
-        target.position = nextPos;
-  
-        Debug.Log("next");
-    }
+        private void Start()
+        {
+            LinkOsc();
 
-    private void backSlide(Transform target, float range)
-    {
-        Vector3 currentPos = target.position;
-        Vector3 backPos = new Vector3(currentPos.x, currentPos.y, currentPos.z - range);
-        target.position = backPos;
-        
-        Debug.Log("back");
-    }
+            this.UpdateAsObservable()
+                .Where(_ => _touch.y > 0 && _touchCount == 1)
+                .ThrottleFirst(TimeSpan.FromSeconds(0.2))
+                .Subscribe(_ => NextSlide(_targetPos.transform, moveRange));
 
-    private void moveCamRoot(Transform root, Transform target)
-    {
-        root.position = Vector3.Lerp(root.position, target.position, Time.deltaTime * moveSpeed);
-    }
+            this.UpdateAsObservable()
+                .Where(_ => _touch.y < 0 && _touchCount == 1)
+                .ThrottleFirst(TimeSpan.FromSeconds(0.2))
+                .Subscribe(_ => BackSlide(_targetPos.transform, moveRange));
 
-    private void linkOsc()
-    {
-        _server = new OscServer(6000); // Port number
-        
-        _server.MessageDispatcher.AddCallback(
-            "/ZIGSIM/mikipomaid/touch0",
-            (string address, OscDataHandle data) =>
-            {
-                _touch.x = data.GetElementAsFloat(0);
-                _touch.y = data.GetElementAsFloat(1);
-            }
-        );
-        
-        _server.MessageDispatcher.AddCallback(
-            "/ZIGSIM/mikipomaid/touchcount",
-            (string address, OscDataHandle data) =>
-            {
-                _touchCount = data.GetElementAsInt(0);
-            }
-        );
+            this.UpdateAsObservable()
+                .Subscribe(_ => MoveCamRoot(camRoot, _targetPos.transform));
+        }
+
+        private static void NextSlide(Transform target, float range)
+        {
+            Vector3 currentPos = target.position;
+            Vector3 nextPos = new Vector3(currentPos.x, currentPos.y, currentPos.z + range);
+            target.position = nextPos;
+            //Debug.Log("next");
+        }
+
+        private static void BackSlide(Transform target, float range)
+        {
+            Vector3 currentPos = target.position;
+            Vector3 backPos = new Vector3(currentPos.x, currentPos.y, currentPos.z - range);
+            target.position = backPos;
+            //Debug.Log("back");
+        }
+
+        private void MoveCamRoot(Transform root, Transform target)
+        {
+            root.position = Vector3.Lerp(root.position, target.position, Time.deltaTime * moveSpeed);
+        }
+
+        private void LinkOsc()
+        {
+            _server = new OscServer(6000);
+
+            _server.MessageDispatcher.AddCallback(
+                "/ZIGSIM/mikipomaid/touch0",
+                (string address, OscDataHandle data) =>
+                {
+                    //_touch.x = data.GetElementAsFloat(0);
+                    _touch.y = data.GetElementAsFloat(1);
+                }
+            );
+
+            _server.MessageDispatcher.AddCallback(
+                "/ZIGSIM/mikipomaid/touchcount",
+                (string address, OscDataHandle data) => { _touchCount = data.GetElementAsInt(0); }
+            );
+        }
     }
 }
