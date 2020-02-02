a0cfb6033d7f6b57163b11e69201e407ca24a4c8
Mikipomaid <cakemas0227@icloud.com>
2020-02-02 19:04:41 +0900

    wip customeditor

---
+++ unityproject/Assets/InteractivePresentation/scripts/Editor/CreateSlides.cs
@@ -0,0 +1,17 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+namespace InteractivePresentation.scripts.Editor
+{
+    public class CreateSlides : EditorWindow
+    {
+        [MenuItem("InteractivePresentation/CreateSlides")]
+        public static void Create()
+        {
+            GameObject newGameObject = new GameObject("New GameObject");
+            Undo.RegisterCreatedObjectUndo(newGameObject, "Create New GameObject");
+        }
+    }
+}
